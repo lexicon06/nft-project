baa36f858591ac62d335dc4cb4661fdb444f139f
Pablo <pablo.hehe2@gmail.com>
2023-12-17 20:03:23 -0300

    added morgan dev

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ import path from "path";
 
 const app = express();
 app.use(express.json());
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
+//if (process.env.NODE_ENV === "development") {
+app.use(morgan("dev"));
+//}
 
 const currentDirectory = process.cwd();
 
