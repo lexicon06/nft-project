f01f19db0b63918ab0baa23aaf1a439ed6cb0ae9
Pablo <pablo.hehe2@gmail.com>
2023-12-16 23:36:55 -0300

    added mongoose new 5.x.x ver and implemented some Schema

--- server.js
+++ server.js
@@ -1,7 +1,56 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DB.replace(/<PASSWORD>/gi, process.env.DB_PASSWORD);
+
+const nftSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Plase enter a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter a price"],
+  },
+});
+
+const NFT = mongoose.model("NFT", nftSchema);
+
+const testNFT = new NFT({
+  name: "Jojo's Bizzare Adventure",
+  rating: 7.8,
+  price: 515,
+});
+
+testNFT
+  .save()
+  .then((docNFT) => {
+    console.log(docNFT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+mongoose
+  .connect(DB, {
+    userCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log(`DB connection successful ${con.connection.host}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //console.log(process.env);
 
